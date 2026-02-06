875077f7827b165637004491e2c820887a23ca03
Andrei Abena <abenaandrei52@gmail.com>
2026-02-06 19:26:37 +0800

    Fix comparison variable names for clarity

---
+++ comparison.py
@@ -0,0 +1,15 @@
+print("Comparison Operators")
+x=int(input("X: "))
+y=int(input("Y: "))
+xgty=x>y
+xgtety=x>=y
+xlty=x<y
+xltety=x<=y
+xey=x==y
+xney=x!=y
+print("x>y: "+str(xgty))
+print("x>=y: "+str(xgtety))
+print("x<y: "+ str(xlty))
+print("x<=y: "+str(xltety))
+print("x==y: "+str(xey))
+print("x!=y: "+str(xney))
