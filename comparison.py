0d76fe46d46749a654d03d61c2b4e1fa31e158f3
Andrei Abena <abenaandrei52@gmail.com>
2026-02-06 19:50:53 +0800

    comparison.py

--- comparison.py
+++ comparison.py
@@ -7,9 +7,9 @@ xlty=x<y
 xltety=x<=y
 xey=x==y
 xney=x!=y
-print("x>y: "+str(xgty))
-print("x>=y: "+str(xgtety))
-print("x<y: "+ str(xlty))
-print("x<=y: "+str(xltety))
-print("x==y: "+str(xey))
-print("x!=y: "+str(xney))
+print(str(x)+">"+str(y)+":"+str(xgty))
+print(str(x)+">="+str(y)+":"+str(xgtety))
+print(str(x)+"<"+str(y)+":"+str(xlty))
+print(str(x)+"<="+str(y)+":"+str(xltety))
+print(str(x)+"=="+str(y)+":"+str(xey))
+print(str(x)+"!="+str(y)+":"+str(xney))
