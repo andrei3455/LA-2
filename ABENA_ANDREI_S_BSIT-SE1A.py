69f3a0c6e92e8642792a2766c040f470b05c25a9
Andrei Abena <abenaandrei52@gmail.com>
2026-02-07 11:36:01 +0800

    Fix print statements to include equals sign

--- ABENA_ANDREI_S_BSIT-SE1A.py
+++ ABENA_ANDREI_S_BSIT-SE1A.py
@@ -7,9 +7,9 @@ xlty=x<y
 xltety=x<=y
 xey=x==y
 xney=x!=y
-print(f"{x} > {y}: {xgty}")
-print(f"{x} >= {y}: {xgtety}")
-print(f"{x} < {y}: {xlty}")
-print(f"{x} <= {y}: {xltety}")
-print(f"{x} == {y}: {xey}")
-print(f"{x} != {y}: {xney}")
+print(f"{x} > {y} = {xgty}")
+print(f"{x} >= {y} = {xgtety}")
+print(f"{x} < {y} = {xlty}")
+print(f"{x} <= {y} = {xltety}")
+print(f"{x} == {y} = {xey}")
+print(f"{x} != {y} = {xney}")
